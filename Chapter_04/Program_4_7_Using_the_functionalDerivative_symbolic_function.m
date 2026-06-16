4dfd5b00f8b8ee722990bc3f7b96813822accf65
Peng Lv <30851498+alvpeng@users.noreply.github.com>
2026-06-16 10:03:15 +0800

    Delete Chapter_04 directory

--- Chapter_04/Program_4_7_Using_the_functionalDerivative_symbolic_function.m
+++
@@ -1,8 +0,0 @@
-% 例程 4.7  使用 functionalDerivative 符号函数求解问题
-%  求解最速降线微分方程
-%  借助符号函数functionalDerivative实现
-syms  g  y(x)
-assume(g, 'positive')
-f = sqrt((1 + diff(y)^2)/(2*g*y));
-eqn = functionalDerivative(f, y) == 0;
-eqn = simplify(eqn)
