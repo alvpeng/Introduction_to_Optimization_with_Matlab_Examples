0d728bc3c11dfc2500f2254012317a7681e12d03
Peng Lv <30851498+alvpeng@users.noreply.github.com>
2026-06-16 10:02:07 +0800

    Delete Chapter_08 directory

--- Chapter_08/Program_8_7_Another_example_of_using_intlinprog_MATLAB_function.m
+++
@@ -1,9 +0,0 @@
-% 例程 8.7  使用 MATLAB 函数 intlinprog () 的另一个示例
-f = [-5, -8];
-intcon = 1:2;
-A = [1, 1; 5, 9];
-b = [6; 45];
-lb = [0, 0];
-ub = [Inf, Inf];
-X = intlinprog(f, intcon, A, b, [], [], lb, ub);
-X
